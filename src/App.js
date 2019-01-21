97e76b0ecea34c78e9bc0473655eb5fcfddbd288
revdfdev <mkodekar@zoho.com>
2019-01-21 17:18:57 +0530

    Initialize context with hooks

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React, { useContext } from "react";
+import { UserContext } from "./index";
+
+export default function App() {
+  const value = useContext(UserContext);
+  return <div>Hello {value}</div>;
+}
