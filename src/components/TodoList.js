0be6ee4693de5652a5aba5ea5878a593c57fadc4
revdfdev <mkodekar@zoho.com>
2019-01-22 01:17:41 +0530

    Add Toggle Todos and Remove todo reducer

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,14 +2,44 @@ import React, { useContext } from "react";
 import TodoContext from "../context";
 
 export default function TodoList() {
-  const { state } = useContext(TodoContext);
+  const { state, dispatch } = useContext(TodoContext);
+  const title =
+    state.todos.length > 0 ? `${state.todos.length} Todos` : "Nothing To Do!";
   return (
-    <div>
-      <ul>
+    <div className="container mx-auto max-w-md text-center font-mono">
+      <h1 className="text-bold">{title}</h1>
+      <ul className="list-reset text-white p-0">
         {state.todos.map(todo => {
           return (
-            <li key={todo.id}>
-              <span>{todo.text}</span>
+            <li
+              key={todo.id}
+              className="flex items-center bg-orange-dark border-black border-dashed border-2 my-2 py-4"
+            >
+              <span
+                onDoubleClick={() =>
+                  dispatch({ type: "TOGGLE_TODO", payload: todo })
+                }
+                className={`flex-1 ml-12 cursor-pointer ${todo.complete &&
+                  "line-through text-grey-darkest"}`}
+              >
+                {todo.text}
+              </span>
+              <button>
+                <img
+                  src="https://icon.now.sh/edit/0050c5"
+                  alt="Edit Icon"
+                  className="h-6"
+                />
+              </button>
+              <button
+                onClick={() => dispatch({ type: "REMOVE_TODO", payload: todo })}
+              >
+                <img
+                  src="https://icon.now.sh/delete/8b0000"
+                  alt="Delete Icon"
+                  className="h-6"
+                />
+              </button>
             </li>
           );
         })}
